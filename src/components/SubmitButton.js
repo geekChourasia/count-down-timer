ba09e53f3f3c79e03bdd1938d7589514677207dc
geekchourasia <engineer.chourasia@gmail.com>
2021-06-20 22:30:02 +0530

    first commit

---
+++ src/components/SubmitButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SubmitButton = ({ datePassed }) => {
+    return ( <button id = "submitbutton"
+        onClick = { datePassed } > submit </button>
+    );
+}
+export default SubmitButton;
